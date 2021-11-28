76e3bdc82d0fb68b72f7fc7f704daa501c7ff0ca
Shmuli Margulies <shmuli9@gmail.com>
2021-11-28 23:33:18 +0000

    reformatted some files
    Signed-off-by: Shmuli Margulies <shmuli9@gmail.com>

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import '../assets/styles/App.css';
+import Results from './Results';
 import Search from './Search';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <Search/>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Search/>
+                <Results/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
