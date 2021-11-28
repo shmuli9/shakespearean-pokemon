ff08cf351d1376a00d1d2db38bc0e9d73659ee6a
Shmuli Margulies <shmuli9@gmail.com>
2021-11-28 23:33:06 +0000

    added basic display of results
    Signed-off-by: Shmuli Margulies <shmuli9@gmail.com>

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -2,17 +2,21 @@ import React, {FormEvent, useState} from 'react';
 
 const Search = () => {
     const [searchQuery, setSearchQuery] = useState("")
+    const [flavorText, setFlavorText] = useState("")
 
     const handleSearch = (e: FormEvent) => {
         e.preventDefault()
 
         fetch(`https://pokeapi.co/api/v2/pokemon-species/${searchQuery}/`)
             .then(r => r.json())
-            .then(r => console.log(r))
+            .then(r => {
+                setFlavorText(r.flavor_text_entries[0].flavor_text)
+            })
             .catch(e => console.log(e))
     }
 
     return <form onSubmit={handleSearch}>
+        {flavorText && <input type="text" disabled value={flavorText}/>}
         <input className={"search-box"}
                placeholder={"Search a Pokemon"}
                value={searchQuery}
