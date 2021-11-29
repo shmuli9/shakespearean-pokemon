46b549b662f35b46c76c47228f5fa466436419b7
Shmuli Margulies <shmuli9@gmail.com>
2021-11-29 00:15:49 +0000

    added initial shakespeare api fn
    Signed-off-by: Shmuli Margulies <shmuli9@gmail.com>

--- client/src/components/Results.tsx
+++ client/src/components/Results.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const Results = (props: { data: string[] }) => {
     const {data} = props
+
+    const shakespearianApi = (text: string) => {
+        fetch(`https://api.funtranslations.com/translate/shakespeare.json?text=${encodeURI(text.replace(/\n|\r/g, " "))}`)
+            .then(r => r.json())
+            .then(console.log)
+            .catch(console.log)
+    }
+
+    useEffect(() => {
+        data.map(flavorText => shakespearianApi(flavorText))
+    }, [data])
 
     return <>
         {data.map((flavorText, index) =>
